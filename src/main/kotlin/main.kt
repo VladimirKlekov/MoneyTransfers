cc945d167da8dee6ac87fe81ae476424fdcd5be6
VladimirKlekov <99707346+VladimirKlekov@users.noreply.github.com>
2022-03-27 21:57:33 +0300

    Сделал немного больше, чем в задании. Буду признателен, если укажите на ошибки

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,70 @@
+const val PERCENT_MASTER_AND_MAESTRO = 0.6
+const val PERCENT_VISA_AND_MIR = 0.75
+const val PERCENT_VK_PAY = 0.00
+const val RUB_LIMIT_CARD_IN_DAY = 150_000
+const val RUB_MIN_LIMIT_VISA_MIR = 35
+const val RUB_LIMIT_VK_PAY_ONCE = 15_000
+
+fun main() {
+
+    while (true) {
+        println("Выберите тип карты:")
+        println("1. Mastercard and Maestro")
+        println("2. Visa and Мир")
+        println("3. VK pay")
+        val chek = readln()
+        if (chek == "end") {
+            println("Программа завершена!")
+            break
+        }
+
+        println("Введите сумму перевода в рублях:")
+        val amount: Int = readLine()!!.toInt()
+
+
+        val input = chek?.toInt()
+
+        when (input) {
+            1 -> result_Mastercard_And_Maestro(amount)
+            2 -> result_Viza_And_Mir(amount)
+            3 -> result_VK_Pay(amount)
+            else -> {
+                print("Ошибка ввода. Выберите правильный вариант")
+            }
+        }
+
+    }
+
+
+}
+
+fun result_Mastercard_And_Maestro(amount: Int) {
+    val result =
+        if (RUB_LIMIT_CARD_IN_DAY > amount) amount * 100 / 100 * PERCENT_MASTER_AND_MAESTRO + 2000 else print("Операция не может быть совершена")
+    println("Перевод соверен. Комиссия составила $result копеек")
+    println("")
+}
+
+fun result_Viza_And_Mir(amount: Int) {
+    val result =
+        if (RUB_LIMIT_CARD_IN_DAY > amount && RUB_MIN_LIMIT_VISA_MIR < amount) amount * 100 / 100 * PERCENT_VISA_AND_MIR + 3500 else print(
+            "Операция не может быть совершена"
+        )
+    println("Перевод соверен. Комиссия составила $result копеек")
+    println("")
+}
+
+fun result_VK_Pay(amount: Int) {
+    val result =
+        if (RUB_LIMIT_VK_PAY_ONCE > amount) amount * 100 / 100 * PERCENT_VK_PAY else print("Операция не может быть совершена")
+    println("Перевод соверен. Комиссия составила $result копеек")
+    println("")
+}
+
+
+
+
+
+
+
+
